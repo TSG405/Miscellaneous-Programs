0f6fde159d1d59f2f64f8d06a6bfa2fe2a6ad561
Tanmoy Sengupta <65300045+TSG405@users.noreply.github.com>
2020-10-29 14:41:14 +0530

    Update Maximum Sliding Window.py

--- Python/Maximum Sliding Window.py
+++ Python/Maximum Sliding Window.py
@@ -1,6 +1,5 @@
 '''
 Sliding Window Maximum (Maximum of all subarrays of size k)
-
 Given an array and an integer K, find the maximum for each and every contiguous subarray of size k.
 
 Examples : 
@@ -12,17 +11,11 @@ Output: 3 3 4 5 5 5 6
 Explanation: 
 
 Maximum of 1, 2, 3 is 3
-
 Maximum of 2, 3, 1 is 3
-
 Maximum of 3, 1, 4 is 4
-
 Maximum of 1, 4, 5 is 5
-
 Maximum of 4, 5, 2 is 5 
-
 Maximum of 5, 2, 3 is 5
-
 Maximum of 2, 3, 6 is 6
 
 Input: arr[] = {8, 5, 10, 7, 9, 4, 15, 12, 90, 13}, K = 4 
@@ -30,12 +23,7 @@ Input: arr[] = {8, 5, 10, 7, 9, 4, 15, 12, 90, 13}, K = 4
 Output: 10 10 10 15 15 90 90
 
 Explanation:
-
-Maximum of first 4 elements is 10, similarly for next 4 
-
-elements (i.e from index 1 to 4) is 10, So the sequence 
-
-generated is 10 10 10 15 15 90 90
+Maximum of first 4 elements is 10, similarly for next 4 elements (i.e from index 1 to 4) is 10, So the sequence generated is 10 10 10 15 15 90 90
 '''
 
 n=int(input())
